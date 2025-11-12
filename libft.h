52fa9fa336541ad46da96afc89e408270f41d55e
Sasha Fedota <ofedota@student.42prague.com>
2025-11-12 18:42:43 +0100

    ft_memcpy has added

--- libft.h
+++ libft.h
@@ -9,5 +9,6 @@ int     ft_isprint(int n);
 size_t  ft_strlen(const char *str);
 void    *ft_memset(void *s, int c, size_t len);
 void    ft_bzero(void *s, size_t len);
+void    *ft_memcpy(void *dest, const void *src, size_t n);
 
 #endif // LIBFT_H
