b172df5e5faf29c35d0a41015a48f24e38aec7d4
Sasha Fedota <ofedota@student.42prague.com>
2025-11-16 20:52:20 +0100

    ft_tolower, ft_toupper has added

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ofedota <ofedota@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 16:46:52 by ofedota           #+#    #+#             */
-/*   Updated: 2025/11/16 20:16:36 by ofedota          ###   ########.fr       */
+/*   Updated: 2025/11/16 20:25:39 by ofedota          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,9 @@ void	ft_bzero(void *s, size_t len);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
-size_t strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 
 #endif // LIBFT_H
