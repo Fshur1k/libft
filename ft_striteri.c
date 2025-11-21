d16082965d9b27e09fa11554d57b9229fda08430
Sasha Fedota <ofedota@student.42prague.com>
2025-11-21 19:30:34 +0100

    ft_striteri has completely added

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,15 +6,42 @@
 /*   By: ofedota <ofedota@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 18:16:50 by ofedota           #+#    #+#             */
-/*   Updated: 2025/11/21 18:18:28 by ofedota          ###   ########.fr       */
+/*   Updated: 2025/11/21 19:29:39 by ofedota          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/* void to_upper(unsigned int i, char *c)
+{
+    (void)i;
+    *c = ft_toupper(*c);
+} */
 /**
  * @brief Apply function to each char (in-place, iterate).
  * @param s Input string (modifiable).
  * @param f Function to apply.
  */
-void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	size_t	i;
+
+	i = 0;
+	if (s == NULL)
+		return ;
+	while (s[i] != '\0')
+	{
+		f(i, &s[i]);
+		i++;
+	}
+}
+
+/* 
+int main(void)
+{
+	char str[] = "small string small problem";
+	printf("%s\n", str);
+	ft_striteri(str,to_upper);
+	printf("%s\n", str);
+	return 0;
+} */
