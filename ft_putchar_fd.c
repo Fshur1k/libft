6b74fe7c8d548112d857051d28ff99b8e4ff7547
Sasha Fedota <ofedota@student.42prague.com>
2025-11-21 21:08:05 +0100

    ft_putchar_fd has added

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,11 +6,18 @@
 /*   By: ofedota <ofedota@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 18:17:59 by ofedota           #+#    #+#             */
-/*   Updated: 2025/11/21 18:18:17 by ofedota          ###   ########.fr       */
+/*   Updated: 2025/11/21 20:09:14 by ofedota          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-
-void ft_putchar_fd(char c, int fd);
+/**
+ * @brief Write character to file descriptor.
+ * @param c Character.
+ * @param fd File descriptor.
+ */
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, sizeof(c));
+}
--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: ofedota <ofedota@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 18:20:12 by ofedota           #+#    #+#             */
-/*   Updated: 2025/11/21 18:20:37 by ofedota          ###   ########.fr       */
+/*   Updated: 2025/11/21 21:07:23 by ofedota          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,17 @@
  * @param s String.
  * @param fd File descriptor.
  */
-void ft_putendl_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return ;
+	while (s[i] != '\0')
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+	write(fd, "\n", 1);
+}
