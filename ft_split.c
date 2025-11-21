c8a1b0cfc992b8cac64451aa1c4080707032eb45
Sasha Fedota <ofedota@student.42student.com>
2025-11-21 12:53:44 +0100

    ft_split has added

--- ft_split.c
+++ ft_split.c
@@ -6,15 +6,114 @@
 /*   By: ofedota <ofedota@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/19 21:33:19 by ofedota           #+#    #+#             */
-/*   Updated: 2025/11/19 21:36:15 by ofedota          ###   ########.fr       */
+/*   Updated: 2025/11/21 12:52:14 by ofedota          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+size_t	count_words(const char *s, char c);
+void	write_words(char **arr, const char *s, size_t words, char c);
 /**
  * @brief Split string by delimiter.
- * @param s String to trim.
+ * @param s String to split.
  * @param c Delimiter.
+ * @return Array of strings terminated by NULL.
  */
-char	**ft_split(char const *s, char c);
+char	**ft_split(char const *s, char c)
+{
+	size_t	words;
+
+	if (s == NULL)
+		return NULL;
+	words = count_words(s, c);
+	char **arr = malloc((words + 1) * sizeof(char *));// + 1 cause end of arr is '\0'
+	if (arr == NULL)
+		return NULL;
+	
+	write_words(arr, s, words, c);
+	return (arr);
+}
+
+size_t	count_words(const char *s, char c)
+{
+	size_t	in_word;
+	size_t	count;
+
+	in_word = 0;
+	count = 0;
+	while (*s)
+	{
+		if (*s != c && in_word == 0)
+			{
+				in_word = 1;
+				count++;
+			}
+		else if (*s == c)
+			in_word = 0;
+		s++;
+	}
+	return (count);
+}
+void	write_words(char **arr, const char *s, size_t words, char c)
+{
+	size_t	i;
+	size_t	j;
+	size_t	start;
+	
+	i = 0;
+	j = 0;
+	while (j < words)
+	{
+		while (s[i] == c)
+			i++;
+		start = i;
+		while (s[i] != c && s[i] != '\0')
+			i++;
+		arr[j] = ft_substr(s, start, i - start);
+		if (arr[j] == NULL)
+		{
+			while(j > 0)
+				free(arr[--j]);
+			free(arr);
+			return;
+		}
+		j++;
+		if (s[i] == '\0')
+			break;
+		i++;
+	}
+	arr[j] = NULL;
+}
+
+/* 
+void print_split(char **arr)
+{
+    int i = 0;
+
+    while (arr[i] != NULL)
+    {
+        printf("[%s]\t", arr[i]);
+        i++;
+    }
+}
+void free_split(char **arr)
+{
+    int i = 0;
+
+    while (arr[i] != NULL)
+    {
+        free(arr[i]);
+        i++;
+    }
+	free(arr);
+}
+int main(void)
+{
+	const char str[] = "Thisxisxaxtestxstringx";
+	char c = 'x';
+	char **arr = ft_split(str, c);
+	print_split(arr);
+	free_split(arr);
+	return (0);
+} */
